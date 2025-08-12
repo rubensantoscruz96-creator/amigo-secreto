f75f176031f472ec3fd192c46ed697b927839fae
rubencrz <ruben.santoscruz96@gmail.com>
2025-08-11 19:54:42 -0600

    funcion actualizar lista de amigos

--- app.js
+++ app.js
@@ -4,20 +4,42 @@
 
 let amigos = [];
 
-//
+//Implementa una función para agregar amigos
 function agregarAmigo() {
+    //Capturar el valor del campo de entrada:
     let nombreAmigo = document.getElementById('amigo');
     
-    if (nombreAmigo.value === "") {
+    //Validar la entrada
+    if (nombreAmigo.value === '') {
         alert('Por favor ingresa un nombre');
-    } else {
-        amigos.push(nombreAmigo);
+    } 
+    //Actualizar el array de amigos
+    else {
+        amigos.push(nombreAmigo.value);
+        //Limpiar el campo de entrada
         limpiarCaja();
+        //Función actualizar lista de amigos
+        listaDeAmigos();
         return nombreAmigo;
     }
-    limpiarCaja();
+    
 }
 
+//función limpiar el campo de entrada
 function limpiarCaja() {
     document.querySelector('#amigo').value = '';
+}
+
+//Implementa una función para actualizar la lista de amigos
+function listaDeAmigos() {
+//Obtener el elemento de la lista
+    let listaHTML = document.getElementById('listaAmigos');
+//Limpiar la lista existente
+    listaHTML.innerHTML = '';
+//Iterar sobre el arreglo
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        listaHTML.appendChild(li);
+    }
 }
