5ef4670e29badfde1cc66c9e0b38ff5946797eee
rubencrz <ruben.santoscruz96@gmail.com>
2025-08-11 18:21:12 -0600

    agregar funcion capturar nombre

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Crear un array para almacenar los nombres
+
+let amigos = [];
+
+//
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById('amigo');
+    
+    if (nombreAmigo.value === "") {
+        alert('Por favor ingresa un nombre');
+    } else {
+        amigos.push(nombreAmigo);
+        limpiarCaja();
+        return nombreAmigo;
+    }
+    limpiarCaja();
+}
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
